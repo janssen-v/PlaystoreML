7339ed67c0f191e1c5c45abb0dc92d50d38e3bf4
Vincentius Janssen <janssen.vincentius@gmail.com>
2020-05-17 13:27:08 +0700

    Fixed naming convention

--- winetree/source/main.py
+++ winetree/source/main.py
@@ -1,6 +1,6 @@
 import csv
-import os
-from pathlib import Path
+import os                   # For getting script directory (multi-device support)
+from pathlib import Path    # Used for easy path referencing
 
 this_folder = os.path.dirname(os.path.abspath(__file__))
 p = Path(this_folder)
@@ -10,8 +10,7 @@ array = []
 with open(path, 'r') as file:
     my_reader = csv.reader(file, delimiter=',')
     
-    # Assign data to list
-    for row in my_reader:
+    for row in my_reader:   # Assign data to list
         array.append(row)
 
     train_data = array[1:]
@@ -25,16 +24,12 @@ with open(path, 'r') as file:
 
 training_data = float_list
 
-
-def unique_vals(rows, col):
-    """Find the unique values for a column in a dataset."""
+def unique_vals(rows, col): # Find the unique values for a column in a dataset.
     return set([row[col] for row in rows])
 
-def class_counts(rows):
-    """Counts the number of each type of example in a dataset."""
-    counts = {}  # a dictionary of label -> count.
-    for row in rows:
-        # in our dataset format, the label is always the last column
+def class_counts(rows): # Counts the number of each type of sample in a dataset.
+    counts = {}         # a dictionary of label -> count.
+    for row in rows:    # in our dataset format, the label is always the last column
         label = row[-1]
         if label not in counts:
             counts[label] = 0
@@ -42,31 +37,25 @@ def class_counts(rows):
     return counts
 
 def is_numeric(value):
-    """Test if a value is numeric."""
+    # Test if a value is numeric. 
     return isinstance(value, int) or isinstance(value, float)
 
-
-
 class Question:
-
 
     def __init__(self, column, value):
         self.column = column
         self.value = value
 
-    def match(self, example):
-        # Compare the feature value in an example to the
+    def match(self, sample):
+        # Compares the feature value in an sample to the
         # feature value in this question.
-        val = example[self.column]
+        val = sample[self.column]
         if is_numeric(val):
             return val >= self.value
         else:
             return val == self.value
 
-
-
 def partition(rows, question):
-
     true_rows, false_rows = [], []
     for row in rows:
         if question.match(row):
@@ -76,7 +65,6 @@ def partition(rows, question):
     return true_rows, false_rows
 
 def gini(rows):
-
     counts = class_counts(rows)
     impurity = 1
     for lbl in counts:
@@ -85,81 +73,51 @@ def gini(rows):
     return impurity
 
 def info_gain(left, right, current_uncertainty):
-
     p = float(len(left)) / (len(left) + len(right))
     return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
 
 def find_best_split(rows):
-
-
     best_gain = 0
     best_question = None  
     current_uncertainty = gini(rows)
     n_features = len(rows[0]) - 1  
 
     for col in range(n_features): 
-
         values = set([row[col] for row in rows])  
-
         for val in values: 
-
             question = Question(col, val)
-
             true_rows, false_rows = partition(rows, question)
-
             if len(true_rows) == 0 or len(false_rows) == 0:
                 continue
 
             gain = info_gain(true_rows, false_rows, current_uncertainty)
-
-
+            
             if gain >= best_gain:
                 best_gain, best_question = gain, question
 
     return best_gain, best_question
 
-
 class Leaf:
-
-
     def __init__(self, rows):
         self.predictions = class_counts(rows)
 
-
 class Node:
-
-    def __init__(self,
-                 question,
-                 true_branch,
-                 false_branch):
+    def __init__(self, question, true_branch, false_branch):
         self.question = question
         self.true_branch = true_branch
         self.false_branch = false_branch
 
-
 def build_tree(rows):
-
     gain, question = find_best_split(rows)
-
-
     if gain == 0:
         return Leaf(rows)
 
-
     true_rows, false_rows = partition(rows, question)
-
-
     true_branch = build_tree(true_rows)
-
-
     false_branch = build_tree(false_rows)
-
-
     return Node(question, true_branch, false_branch)
 
 def classify(row, node):
-
-
     if isinstance(node, Leaf):
         return node.predictions
     if node.question.match(row):
@@ -167,21 +125,15 @@ def classify(row, node):
     else:
         return classify(row, node.false_branch)
 
-
 if __name__ == '__main__':
-
     my_tree = build_tree(training_data)
-
     path2 = str(p.parent) + '/test.csv'
-    
     array2 = []
+
     with open(path2, 'r') as file:
         my_reader2 = csv.reader(file, delimiter=',')
-        
-        # Assign data to list
-        for row in my_reader2:
+        for row in my_reader2:   # Assign data to list
             array2.append(row)
-
         test_data = array2[1:]
         pholder2 = []
         float_list2 = []
@@ -190,15 +142,12 @@ if __name__ == '__main__':
             for b in a:
                 b = float(b)
                 pholder2.append(b)
-                
             float_list2.append(pholder2)
     
     testing_data = float_list2
-
     n = 0
     j = 0
 
-    
     for row in testing_data:
         k = list(classify(row,my_tree))[0] > 6
         m = row[-1] > 6
